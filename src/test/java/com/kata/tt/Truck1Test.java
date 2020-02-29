3ec8276642b6f5de48b7e30488ad42ab0b35a468
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-02-29 18:44:39 +0000

    Minor changes

--- src/test/java/com/kata/tt/Truck1Test.java
+++ src/test/java/com/kata/tt/Truck1Test.java
@@ -12,7 +12,6 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
 @RunWith(PowerMockRunner.class)
